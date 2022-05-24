77a51d2fa5f4865eafec810fe200e392bf3055e8
Karthivasanth <Karthivasanth123@gmail.com>
2022-05-24 22:01:14 +0530

    alert added

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
-let currentURL = window.location.href;
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  console.log(response.farewell);
+chrome.storage.local.get(['key'], function(result) {
+  console.log('pred is  read in frontend script' + result.key);
+  var res = Math.floor(result.key*100);
+  if(res<80){
+      alert("This Might be Unsafe Site")
+  }
 });
 
-chrome.runtime.onMessage.addListener(gotmsg);
 
-function gotmsg(request,sender,sendResponse){
-  console.log(request.pred)
-}
+chrome.storage.local.get(['urls'], function(result) {
+  console.log('url is  read in frontend script' + result.urls);
+});
